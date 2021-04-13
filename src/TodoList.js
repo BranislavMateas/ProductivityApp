cd99d86f95de01e1c34e93342b6b861d11af23a8
BranislavMateas <mateas.branislav@gmail.com>
2021-04-13 18:53:34 +0200

    Added key to mapping to render just new to do-s.

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,7 +4,7 @@ import Todo from "./Todo"
 export default function TodoList({todos}) {
   return (
     todos.map(todo => {
-      return <Todo exactTodo={todo}/>
+      return <Todo key={todo} exactTodo={todo}/>
     })
   );
 }
