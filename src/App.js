04d383ff0cb8c6b712a5a662c652b642a4877d51
BranislavMateas <mateas.branislav@gmail.com>
2021-07-04 16:18:35 +0200

    Added datepicker, description and todo is prepared to be set up a date of the deadline.

--- src/App.js
+++ src/App.js
@@ -2,45 +2,57 @@ import React, { useState, useRef, useEffect } from "react";
 import TodoList from "./TodoList";
 import { v4 as uuidv4 } from "uuid";
 import "./myStyles.css";
+import DatePicker from "react-date-picker";
 
 const LOCAL_STORAGE_KEY = "todoApp.todos";
 
 function App() {
-  // date process
-  var initialDate = new Date()
-  var currDay = initialDate.getDay()
+  // date picker
+  const [value, onChange] = useState(new Date());
 
-  switch (currDay){
+  // date process
+  var initialDate = new Date();
+  var currDay = initialDate.getDay();
+
+  switch (currDay) {
     case 0:
-      currDay = "Nedeľa"
+      currDay = "Nedeľa";
       break;
     case 1:
-      currDay = "Pondelok"
+      currDay = "Pondelok";
       break;
     case 2:
-      currDay = "Utorok"
+      currDay = "Utorok";
       break;
     case 3:
-      currDay = "Streda"
+      currDay = "Streda";
       break;
     case 4:
-      currDay = "Štvrtok"
+      currDay = "Štvrtok";
       break;
     case 5:
-      currDay = "Piatok"
-      break;  
-    case 6:
-      currDay = "Sobota"
+      currDay = "Piatok";
       break;
-    
+    case 6:
+      currDay = "Sobota";
+      break;
+
     default:
       break;
   }
 
-  var currDate = currDay + ", " + initialDate.getDate() + "." + (initialDate.getMonth()+1) + "." + initialDate.getFullYear()
+  var currDate =
+    currDay +
+    ", " +
+    initialDate.getDate() +
+    "." +
+    (initialDate.getMonth() + 1) +
+    "." +
+    initialDate.getFullYear();
 
   const [todosList, setTodos] = useState([]); // useState vracia array(2) - object destructuring - jedna polozka aktuálny stav, druhá bude pre funkciu, ktorá tento stav updatuje
   const todoContains = useRef();
+  const todoContainsDes = useRef();
 
   useEffect(() => {
     // make todos load from local storage
@@ -62,55 +74,84 @@ function App() {
 
   function handleAddTodo(e) {
     const todoName = todoContains.current.value;
-    if (todoName === "") return alert("Sorry, ale v tvojom TODO tasku ti chýba obsah!");
+    const todoDescr = todoContainsDes.current.value;
+
+    if (todoName === "")
+      return alert("Sorry, ale v tvojom TODO tasku ti chýba obsah!");
     setTodos((prevTodos) => {
-      return [...prevTodos, { id: uuidv4(), name: todoName, complete: false }];
+      return [
+        ...prevTodos,
+        { id: uuidv4(), name: todoName, complete: false, descr: todoDescr },
+      ];
     });
     todoContains.current.value = null;
+    todoContainsDes.current.value = null;
   }
 
   function handleCompleteTodos(e) {
     setTodos(() => {
-      return todosList.filter((exactTodo) => !exactTodo.complete)
+      return todosList.filter((exactTodo) => !exactTodo.complete);
     });
   }
-  
+
   var adderer = document.getElementById("adder");
-  
+
   //format poctu taskov
-  switch(todosList.filter((exactTodo) => !exactTodo.complete).length) {
+  switch (todosList.filter((exactTodo) => !exactTodo.complete).length) {
     case 1:
-      var tasky="task";
+      var tasky = "task";
       break;
     case 2:
-      tasky="tasky";
+      tasky = "tasky";
       break;
     case 3:
-      tasky="tasky";
+      tasky = "tasky";
       break;
     case 4:
-      tasky="tasky";
+      tasky = "tasky";
       break;
     default:
-      tasky="taskov"
+      tasky = "taskov";
       break;
   }
 
   return (
-     <>
+    <>
       <div className="blok">
-
         <h1 id="date">{currDate}</h1>
-        <h3 id="remaining">Ostáva: {todosList.filter((exactTodo) => !exactTodo.complete).length} {tasky}</h3>
+        <h3 id="remaining">
+          Ostáva: {todosList.filter((exactTodo) => !exactTodo.complete).length}{" "}
+          {tasky}
+        </h3>
         <ul className="list">
-          <TodoList todos={todosList} toggleTodo={toggleTodo} handleCompleteTodos={handleCompleteTodos} />
+          <TodoList
+            todos={todosList}
+            toggleTodo={toggleTodo}
+            handleCompleteTodos={handleCompleteTodos}
+          />
         </ul>
-        <input className="bar" onKeyPress={(e) => e.key === "Enter" && adderer.click()} ref={todoContains} type="text"></input>
+        <input
+          className="bar"
+          onKeyPress={(e) => e.key === "Enter" && adderer.click()}
+          ref={todoContains}
+          type="text"
+        ></input>
+        <input
+          className="bar"
+          onKeyPress={(e) => e.key === "Enter" && adderer.click()}
+          ref={todoContainsDes}
+          type="text"
+        ></input>
         <div id="adder" onClick={handleAddTodo}>
           <a>Pridaj To-Do</a>
         </div>
-         
-      </div>   
+        <DatePicker
+          autoFocus="true"
+          className="piker"
+          onChange={onChange}
+          value={value}
+        />
+      </div>
     </>
   );
 }
--- src/Todo.js
+++ src/Todo.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./myStyles.css";
 
 export default function Todo({ exactTodo, toggleTodo, handleCompleteTodos }) {
+  function remainingDays() {
+
+  }
+  
   function hadleTodoClick() {
     toggleTodo(exactTodo.id);
     handleCompleteTodos()
@@ -16,7 +20,10 @@ export default function Todo({ exactTodo, toggleTodo, handleCompleteTodos }) {
           checked={exactTodo.complete}
           onChange={hadleTodoClick}
         ></input>
-        {exactTodo.name}
+        <h4>{exactTodo.name}</h4>
+        <h6>{exactTodo.descr}</h6>
+        <p>Termín: {exactTodo.date}</p>
+        <p>Zostavajúci čas: {remainingDays}</p>
       </label>
     </div>
   );
