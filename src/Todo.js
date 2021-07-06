ed6594e6a8962945e968e7c4c02afc0ce09e328c
BranislavMateas <mateas.branislav@gmail.com>
2021-07-06 09:52:32 +0200

    Added font awesome icons to my project.

--- src/Todo.js
+++ src/Todo.js
@@ -1,5 +1,10 @@
 import React from "react";
 import "./myStyles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faChevronUp,
+    faChevronDown,
+} from "@fortawesome/fontawesome-free-solid";
 
 export default function Todo({ exactTodo, toggleTodo, handleCompleteTodos }) {
     function remainingDays() {}
@@ -10,23 +15,29 @@ export default function Todo({ exactTodo, toggleTodo, handleCompleteTodos }) {
     }
 
     return (
-        <div className="item">
-            <div className="leftSide">
-                <input
-                    type="checkbox"
-                    className="confirmation"
-                    checked={exactTodo.complete}
-                    onChange={hadleTodoClick}
-                ></input>
-            </div>
+        <>
+            <div className="item">
+                <div className="leftSide">
+                    <input
+                        type="checkbox"
+                        className="confirmation"
+                        checked={exactTodo.complete}
+                        onChange={hadleTodoClick}
+                    ></input>
+                </div>
 
-            <div className="rightSide">
-                <h3>{exactTodo.name}</h3>
-                <h5>
-                    {exactTodo.date} | do {remainingDays}
-                </h5>
-                <h4>{exactTodo.descr}</h4>
+                <div className="rightSide">
+                    <h3>{exactTodo.name}</h3>
+                    <h5>
+                        {exactTodo.date} | do {remainingDays}
+                    </h5>
+                    <h4>{exactTodo.descr}</h4>
+                </div>
             </div>
-        </div>
+            <div>
+                <FontAwesomeIcon icon={faChevronUp} />
+                <FontAwesomeIcon icon={faChevronDown} />
+            </div>
+        </>
     );
 }
